b9c9a9572152a40a665bb056f34e3e2c53088384
Stalin Huallullo <antonyhuallullo@gmai.com>
2020-01-16 20:10:54 -0500

    first commit

---
+++ src/main/java/pe/isil/javademoaron/JavaDemoaronApplication.java
@@ -0,0 +1,13 @@
+package pe.isil.javademoaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoaronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDemoaronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/isil/javademoaron/JavaDemoaronApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.javademoaron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoaronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
